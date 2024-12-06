20c7c2246d34bf22b4e577e2274309b7d9b03658
meghana <meghanachinnam12@gmail.com>
2024-12-06 19:23:14 +0530

    done

--- task-3.js
+++ task-3.js
@@ -21,7 +21,27 @@ WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?appid=8e
 WEATHER_DATA_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?appid=8e8efe9e95e97ba21bda7b5958240579&eclude=miutely&units=metric&";
 
 function findUserLocation() {
-    fetch(WEATHER_API_ENDPOINT+"London")
+    fetch(WEATHER_API_ENDPOINT + userLocation.value)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        if(data.cod!= '' && data.cod!=200){
+            alert(data.message);
+            return;
+        }
+        console.log(data);
+
+        city.innerHTML=data.name + ", "+data.sys.country;
+        weatherIcon.style.background='url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)'
+        fetch(
+            WEATHER_DATA_ENDPOINT + 'lon=${data.coord.lom}&lat=${data.coord.lat}'
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            temparature.innerHTML = data.current.temp;
+            feelslike.innerHTML = "Feels like"+data.current.feels_like;
+            discription.innerHTML='<i class="fa-gands fa-cloudversify"></i> &nbsp:'+data.current.weather[0].discription;
+        });
+    });
 }
