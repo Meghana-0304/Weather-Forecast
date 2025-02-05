ba513dddecab64621f7033989f0ca58126fe5ee3
meghana <meghanachinnam12@gmail.com>
2025-02-06 00:51:39 +0530

    Commit

--- task-3.js
+++ task-3.js
@@ -1,47 +1,50 @@
-const userLocation = document.getElementById("userLocation")
-const converter = document.getElementById("converter")
-const weatherIcon = document.querySelector(".weatherIcon")
-const temparature = document.querySelector(".temparature")
-const feelslike = document.querySelector(".feelslike")
-const discription = document.querySelector(".discription")
-const date = document.querySelector(".date")
-const city = document.querySelector(".city")
-
-const Hvalue = document.getElementById("Hvalue")
-const Wvalue = document.getElementById("Wvalue")
-const SRvalue = document.getElementById("SRvalue")
-const SSvalue = document.getElementById("SSvalue")
-const Cvalue = document.getElementById("Cvalue")
-const UVvalue = document.getElementById("UVvalue")
-const Pvalue = document.getElementById("Pvalue")
-
-const forecast = document.querySelector(".forecast")
-
-WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?appid=8e8efe9e95e97ba21bda7b5958240579&q=";
-WEATHER_DATA_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?appid=8e8efe9e95e97ba21bda7b5958240579&eclude=miutely&units=metric&";
-
-function findUserLocation() {
-    fetch(WEATHER_API_ENDPOINT + userLocation.value)
-    .then((response) => response.json())
-    .then((data) => {
-        if(data.cod!= '' && data.cod!=200){
-            alert(data.message);
-            return;
-        }
-        console.log(data);
-
-        city.innerHTML=data.name + ", "+data.sys.country;
-        weatherIcon.style.background='url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)'
-        fetch(
-            WEATHER_DATA_ENDPOINT + 'lon=${data.coord.lom}&lat=${data.coord.lat}'
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-
-            temparature.innerHTML = data.current.temp;
-            feelslike.innerHTML = "Feels like"+data.current.feels_like;
-            discription.innerHTML='<i class="fa-gands fa-cloudversify"></i> &nbsp:'+data.current.weather[0].discription;
-        });
-    });
-}
+const api = {
+    key: "fcc8de7015bbb202209bbf0261babf4c",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+  
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
+  
+  function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+  }
